d5dc2a4eb3fd80cf8fda4d93ccdb54f160e61fe6
Owanikin <oderindeife@gmail.com>
2022-11-23 16:30:36 +0100

    test

--- test/vault.test.js
+++ test/vault.test.js
@@ -1,3 +1,18 @@
 const { assert, expect } = require("chai")
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Vault Unit Tests", function () {
+        let token, receipt, oracle, vault, user, deployer
+        const chainId = network.config.chainId
+        
+        beforeEach(async () => {
+            deployer = (await getNamedAccounts()).deployer
+            await deployments.fixture(["all"])
+            token = await ethers.getContract("StableCoinToken", deployer)
+            vault = await ethers.getContract("Vault", deployer)
+        })
+
+    })
